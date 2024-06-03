a49f4d52d6cfb5749ed57aabce25c3245d7e2f73
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-06-03 12:26:51 -0700

    oops typo

--- src/sys/unix/uds/socketaddr.rs
+++ src/sys/unix/uds/socketaddr.rs
@@ -12,7 +12,7 @@ use std::{ascii, fmt};
 ///
 /// [`net::SocketAddr`]: std::os::unix::net::SocketAddr
 /// [`accept`]: #method.accept
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct SocketAddr {
     sockaddr: libc::sockaddr_un,
     socklen: libc::socklen_t,
